da463ae62c9811c6721473a3963a3070d919da50
kcmw3e <kcmw3e@gmail.com>
2021-05-04 20:43:29 -0400

    project.m now a function
    project.m is now a function (will add optional args to test just a single file, just a single folder, etc. or default to presumably existing data -- essentially what it does now)

--- project.m
+++ project.m
@@ -1,9 +1,5 @@
-clear;
-close all;
-clc;
-
+% Semester Project Sentiment Analysis based on IMDB movie reviews
 % 24-311 Numerical Methods, Carnegie Mellon University
-% Semester Project
 % Casey Walker
 
 % Training steps
@@ -23,21 +19,66 @@ clc;
 %  remove unwanted words
 %  calculate probabilites based on training data
 
+function project()
+
+clear;
+close all;
+clc;
+
 addpath(fullfile(pwd, 'lib'));
 
-[pvocab, pcount, ptexts, pmap] = train(fullfile(pwd, "data/train/pos"));
-[nvocab, ncount, ntexts, nmap] = train(fullfile(pwd, "data/train/neg"));
-vocab = union(pvocab, nvocab);
+nlprint("Semester Project -- Sentiment Analysis based on IMDB movie reviews");
+nlprint("24-311 Numerical Methods, Carnegie Mellon University");
+nlprint("Casey Walker");
+nlprint();
 
-pP = nb_probs(pvocab, pcount, length(vocab));
-nP = nb_probs(nvocab, ncount, length(vocab));
+nlprint("Checking for file with pre-computed Naive Bayes probabilities.");
+if (~isfile(fullfile(pwd, "nbps.mat")))
+    nlprint("  Couldn't find file containing pre-computed Naive Bayes probabilites.");
+    nlprint("  Computing them now instead using data from IMDB.")
+    
+    nlprint("    Checking that data exists.");
+    check_folder(fullfile(pwd, "data/train/pos"));
+    check_folder(fullfile(pwd, "data/train/neg"));
+    
+    nlprint("    Training Naive Bayes model from existing IMDB review data.");
+    [pvocab, pcount, ptexts, pmap] = train(fullfile(pwd, "data/train/pos"));
+    [nvocab, ncount, ntexts, nmap] = train(fullfile(pwd, "data/train/neg"));
+    vocab = union(pvocab, nvocab);
+    
+    nlprint("    Computing probabilities with data from IMDB reviews.");
+    pP = nb_probs(pvocab, pcount, length(vocab));
+    nP = nb_probs(nvocab, ncount, length(vocab));
+    
+    nlprint("    Saving data for future use.");
+    save('nbps', 'pP', 'nP', 'pcount', 'ncount', 'vocab');
+    
+    nlprint("  Done computing Naive Bayes probabilities.");
+else
+    nlprint("  Found file containing model. Loading data...");
+    load(fullfile(pwd, "nbps"));
+end
+nlprint()
 
-nlprint("now testing");
 
+nlprint("Testing model.");
+
+nlprint("  Checking for existence of proper folders.");
+check_folder(fullfile(pwd, "data/test/pos"));
+check_folder(fullfile(pwd, "data/test/neg"));
+check_folder(fullfile(pwd, "data/test/non"));
+
+nlprint("  Running tests.");
 pacc = test(fullfile(pwd, "data/test/pos"), pcount, pP, ncount, nP, length(vocab));
 nacc = test(fullfile(pwd, "data/test/neg"), pcount, pP, ncount, nP, length(vocab));
 nonacc = test(fullfile(pwd, "data/test/non"), pcount, pP, ncount, nP, length(vocab));
+nlprint("  Finished testing.");
 
-nlprint("Pos: %g%%, Neg: %g%%, Non: %g%%", pacc*100, nacc*100, nonacc*100);
+nlprint("Results:");
+nlprint("  Positive review accuracy: %g%%", pacc*100);
+nlprint("  Negative review accuracy: %g%%", nacc*100);
+nlprint("  None-review accuracy:     %g%%", nonacc*100);
 
 rmpath(fullfile(pwd, 'lib'));
+
+end
